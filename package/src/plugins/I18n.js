7e768ebb1d5d39d86fde24415d54af846a71a854
Daniele 'px' Picca <d.picca@sevenia.tech>
2021-08-29 22:48:06 +0200

    Trying to fix I18n plugin

--- package/src/plugins/I18n.js
+++ package/src/plugins/I18n.js
@@ -1,23 +1,23 @@
 import { setLocale } from 'yup'
 
-setLocale({
-    mixed: {
-        default: () => ({ key: 'error__field_invalid' }),
-        required: () => ({ key: 'error__field_required' }),
-    },
-    number: {
-        min: ({ min }) => ({ key: 'error__number_too_small', values: { min } }),
-        max: ({ max }) => ({ key: 'error__number_too_big', values: { max } }),
-    },
-    string: {
-        min: ({ min }) => ({ key: 'error__string_too_short', values: { min } }),
-        max: ({ max }) => ({ key: 'error__string_too_big', values: { max } }),
-    },
-})
-
 export default {
     plugin: null,
     create: function (cfg) {
+        setLocale({
+            mixed: {
+                default: () => ({ key: 'error__field_invalid' }),
+                required: () => ({ key: 'error__field_required' }),
+            },
+            number: {
+                min: ({ min }) => ({ key: 'error__number_too_small', values: { min } }),
+                max: ({ max }) => ({ key: 'error__number_too_big', values: { max } }),
+            },
+            string: {
+                min: ({ min }) => ({ key: 'error__string_too_short', values: { min } }),
+                max: ({ max }) => ({ key: 'error__string_too_big', values: { max } }),
+            },
+        })
+
         this.plugin = {
             locales: cfg.locales,
             translations: {},

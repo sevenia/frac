74ab80503823c41d6ab63b39f44befcfcfd30e83
Daniele 'px' Picca <d.picca@sevenia.tech>
2021-08-29 22:57:37 +0200

    Trying to fix I18n plugin

--- package/src/plugins/I18n.js
+++ package/src/plugins/I18n.js
@@ -1,23 +1,8 @@
-import { setLocale } from 'yup'
+import * as yup from 'yup'
 
 export default {
     plugin: null,
     create: function (cfg) {
-        setLocale({
-            mixed: {
-                default: () => ({ key: 'error__field_invalid' }),
-                required: () => ({ key: 'error__field_required' }),
-            },
-            number: {
-                min: ({ min }) => ({ key: 'error__number_too_small', values: { min } }),
-                max: ({ max }) => ({ key: 'error__number_too_big', values: { max } }),
-            },
-            string: {
-                min: ({ min }) => ({ key: 'error__string_too_short', values: { min } }),
-                max: ({ max }) => ({ key: 'error__string_too_big', values: { max } }),
-            },
-        })
-
         this.plugin = {
             locales: cfg.locales,
             translations: {},
@@ -55,6 +40,21 @@ export default {
                         .querySelector('html')
                         .setAttribute('lang', locale.code)
                 }
+
+                yup.setLocale({
+                    mixed: {
+                        default: () => ({ key: 'error__field_invalid' }),
+                        required: () => ({ key: 'error__field_required' }),
+                    },
+                    number: {
+                        min: ({ min }) => ({ key: 'error__number_too_small', values: { min } }),
+                        max: ({ max }) => ({ key: 'error__number_too_big', values: { max } }),
+                    },
+                    string: {
+                        min: ({ min }) => ({ key: 'error__string_too_short', values: { min } }),
+                        max: ({ max }) => ({ key: 'error__string_too_big', values: { max } }),
+                    },
+                })
             },
             translate: (key) => {
                 if (typeof key === 'string') {
